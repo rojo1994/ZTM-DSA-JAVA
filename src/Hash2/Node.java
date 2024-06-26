3d5153d77e87ea27d300340e0c04af7513b88b30
rojo1994 <rodri-1994-s2@hotmail.com>
2024-06-26 07:58:25 -0700

    Node Hash Table

---
+++ src/Hash2/Node.java
@@ -0,0 +1,32 @@
+package Hash2;
+
+public class Node {
+
+    private String key;
+    private int value;
+
+    public Node(String key, int value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey(){
+        return key;
+    }
+
+
+    public void setKey(String key){
+        this.key = key;
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    public void setValue(int value){
+        this.value = value;
+
+    }
+
+
+}
